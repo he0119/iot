53d4690cb1d5bb4a826fdb799af6720f1d113579
Mengyang He <hmy0119@gmail.com>
2018-03-21 19:22:44 +0800

    send json data to server

--- mqtt.py
+++ mqtt.py
@@ -1,4 +1,6 @@
 import logging
+from datetime import datetime
+import json
 
 import paho.mqtt.client as mqtt
 import requests
@@ -9,7 +11,8 @@ SERVER_USERNAME = config.get_config("server", "username")
 SERVER_PASSWORD = config.get_config("server", "password")
 MQTT_USERNAME = config.get_config("mqtt", "username")
 MQTT_PASSWORD = config.get_config("mqtt", "password")
-
+API_URL = "http://127.0.0.1:5000/api/status"
+BROKER_SERVER = "broker.shiftr.io"
 
 def on_connect(client, userdata, flags, rc):
     '''The callback for when the client receives a CONNACK response from the server.'''
@@ -23,8 +26,18 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     '''The callback for when a PUBLISH message is received from the server.'''
     # print(msg.topic + " " + msg.payload.decode())
+    device_data = msg.payload.decode().split(",")
+    json_data = json.dumps({"code" : int(device_data[0]),
+                            "temperature" : float(device_data[1]),
+                            "relative_humidity" : float(device_data[2]),
+                            "relay1" : True if device_data[3] == "ON" else False,
+                            "relay2" : True if device_data[4] == "ON" else False,
+                            "time" : device_data[5]})
+    print(json_data)
     try:
-        res = requests.post('http://127.0.0.1:5000/api/status', data=msg.payload.decode(), auth=HTTPBasicAuth(SERVER_USERNAME, SERVER_PASSWORD))
+        res = requests.post(API_URL,
+                            data=json_data,
+                            auth=HTTPBasicAuth(SERVER_USERNAME, SERVER_PASSWORD))
         if res.json()["status"] == "failed":
             print("failed")
     except requests.exceptions.ConnectionError:
@@ -38,7 +51,7 @@ def main():
     client.on_connect = on_connect
     client.on_message = on_message
     client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
-    client.connect("broker.shiftr.io")
+    client.connect(BROKER_SERVER)
     client.loop_forever()
 
 if __name__ == '__main__':

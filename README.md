f03b27ccc330310b77c2f39105990913ca3d6806
Mengyang He <hmy0119@gmail.com>
2018-03-31 08:57:35 +0800

    Using Flask Command Line Interface

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 Simple try for home automation using Python, Flask, Angular, MQTT, Arduino
 
 ## Requirements
-* Python 3.6.4
+* Python 3.6.5
 * Node.js 8.10.0
 
 ## Getting Started
 1. Setup python environment
-    ```
+    ```bash
     virtualenv --no-site-packages venv
-    source venv/bin/activite
+    source venv/bin/activite(venv\Scripts\activate on Windows)
     pip install -r requirements.txt
     ```
 2. Setup angular environment
 	
-	```
+	```bash
     cd iot\angular
     npm install -g @angular/cli
 	npm install
@@ -31,7 +31,7 @@ Simple try for home automation using Python, Flask, Angular, MQTT, Arduino
     email=hello@example.com
 
     [mqtt]
-    #I current use broker.shiftr.io
+    #I currently use shiftr.io
     id=mqtt client id
     username=broker username
     password=broker password
@@ -39,15 +39,15 @@ Simple try for home automation using Python, Flask, Angular, MQTT, Arduino
     port=8883
     ```
     - Init database
-    ```
+    ```bash
     export FLASK_APP=run.py(set FLASK_APP=run.py on Windows)
     flask db init
     flask db migrate -m "init db"
     flask db upgrade
-    python run.py createaccount
+    flask createaccount
     ```
 4. Start server
-    ```
+    ```bash
     python run.py
     ```
 Now you can go to http://127.0.0.1:5000/
--- run.py
+++ run.py
@@ -1,22 +1,25 @@
 '''Flask App'''
 import sys
 
+import click
+
 from config import Config
 from iot import create_app, db
 from iot.models.user import User
 
 app = create_app(Config)
 
+@app.cli.command()
+def createaccount():
+    with app.app_context():
+        if db.session.query(User).filter(User.username == Config.ADMIN_USERNAME).first():
+            print('You have already created a user')
+        else:
+            new_user = User(username=Config.ADMIN_USERNAME, email=Config.ADMIN_EMAIL)
+            new_user.set_password(Config.ADMIN_PASSWORD)
+            db.session.add(new_user)
+            db.session.commit()
+            print('Account Created')
+
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        app.run(threaded=True) #Fix Slow Requests on Local Flask Server
-    else:
-        with app.app_context():
-            if db.session.query(User).filter(User.username == Config.ADMIN_USERNAME).first():
-                print('You have already created a user')
-            else:
-                new_user = User(username=Config.ADMIN_USERNAME, email=Config.ADMIN_EMAIL)
-                new_user.set_password(Config.ADMIN_PASSWORD)
-                db.session.add(new_user)
-                db.session.commit()
-                print('Account Created')
+    app.run(threaded=True) #Fix Slow Requests on Local Flask Server

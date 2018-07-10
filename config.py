799d0ebb40a54b996f2b8eebb26ae48c62bb19bd
Mengyang He <hmy0119@gmail.com>
2018-07-10 08:59:17 +0800

    Fix the .env load problem when using python run.py

--- config.py
+++ config.py
@@ -1,12 +1,18 @@
 '''config'''
 import os
+from pathlib import Path  # python3 only
 
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
 class Config(object):
     '''config'''
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(
         os.path.abspath(os.path.dirname(__file__)), 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
     SWAGGER_URL = '/api/docs'
     SWAGGER_API_URL = 'http://127.0.0.1:5000/static/swagger.json'

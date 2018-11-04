17cc678218c2596d46eecbda086e8f2af563ba92
Mengyang He <hmy0119@gmail.com>
2018-11-04 19:51:00 +0800

    Add watchdog and ability to save config

--- arduino/esp8266/autoIrrigation/autoIrrigation.ino
+++ arduino/esp8266/autoIrrigation/autoIrrigation.ino
@@ -1,21 +1,27 @@
-// WIFI&OTA
+//WIFI&OTA&FS
 #include <ESP8266WiFi.h>
 #include <ArduinoOTA.h>
+#include <FS.h>
 
-// Config
+//Ticker&Watchdog
+#include <Ticker.h>
+Ticker secondTick;
+volatile int watchdogCount = 0;
+
+//Config
 #include "config.h"
 
-// NTP
+//NTP
 #include <NTPClient.h>
 #include <WiFiUdp.h>
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "ntp.aliyun.com", 0, 60000);
-// FIXME:莫名奇妙会无法与NTP服务器同步时间。(猜测是和路由器有关)
+//FIXME:莫名奇妙会无法与NTP服务器同步时间。(猜测是和路由器有关)
 
-// Json
+//Json
 #include <ArduinoJson.h>
 
-// Socket
+//Socket
 #include <SocketIoClient.h>
 #if ENABLE_SSL
 #define beginwebsocket beginSSL
@@ -24,43 +30,53 @@ NTPClient timeClient(ntpUDP, "ntp.aliyun.com", 0, 60000);
 #endif
 SocketIoClient webSocket;
 
-// DHT
+//DHT
 #include <dht.h>
 #if DHT_VER == 11
 #define readdht read11
 #elif DHT_VER == 22
 #define readdht read22
 #endif
-#define DHT_PIN D4 // 连接到DHT传感器的端口
+#define DHT_PIN D4 //连接到DHT传感器的端口
 dht DHT;
 
-// Status
+//Status
 char msg[100];
+
 unsigned long lastMillis = 0;  //计时器
 unsigned long valveMillis = 0; //电磁阀自动关闭计时器
 bool valve_delay_trigger = 0;  //电磁阀自动关闭触发器
 unsigned long pumpMillis = 0;  //抽水机自动关闭计时器
 bool pump_delay_trigger = 0;   //抽水机自动关闭触发器
+bool humidity_trigger = 0;     //湿度触发器
+unsigned long soil_humidity = 0;
+unsigned long soil_wet_limit = 1023; //湿度触发阈值
 
 String temperature = "";
 String relative_humidity = "";
+
 bool valve = false;
 bool pump = false;
 unsigned long valve_delay = 60; //电磁阀延时，单位 秒
 unsigned long pump_delay = 60;  //抽水机延时，单位 秒
+
 String data_readtime = "";
-#define PUMP_PIN D1  // 抽水机
-#define VALVE_PIN D2 // 电磁阀 (连接继电器的端口)
+#define PUMP_PIN D1          //抽水机
+#define VALVE_PIN D2         //电磁阀 (连接继电器的端口)
+#define SOIL_HUMIDITY_PIN A0 //土壤湿度传感器
+
+bool need_save_config = false; //设置保存触发器
 
 void event(const char *payload, size_t length)
 {
   StaticJsonBuffer<300> jsonBuffer;
   JsonObject &root = jsonBuffer.parseObject(payload);
-  // Test if parsing succeeds.
+  //Test if parsing succeeds.
   if (!root.success())
   {
     return;
   }
+
   if (root["valve"] != "null")
   {
     valve = root["valve"];
@@ -79,12 +95,30 @@ void event(const char *payload, size_t length)
       pumpMillis = millis(); //重置抽水机关闭计时
     }
   }
+
   if (root["valve_delay"] != "null")
+  {
     valve_delay = root["valve_delay"];
+    need_save_config = true;
+  }
   if (root["pump_delay"] != "null")
+  {
     pump_delay = root["pump_delay"];
+    need_save_config = true;
+  }
+  if (root["soil_wet_limit"] != "null")
+  {
+    soil_wet_limit = root["soil_wet_limit"];
+    need_save_config = true;
+  }
+
   event_set();
-  upload();
+  upload(0);
+  if (need_save_config)
+  {
+    save_config();
+    need_save_config = false;
+  }
 }
 
 void event_set()
@@ -121,26 +155,99 @@ void read_data()
     temperature = "Error";
     break;
   }
-  data_readtime = timeClient.getEpochTime(); //读取数据的时间
+  soil_humidity = analogRead(SOIL_HUMIDITY_PIN); //雨水和湿度情况
+  data_readtime = timeClient.getEpochTime();     //读取数据的时间
 }
 
-void upload()
+void upload(bool reset)
 {
   String payload = String("{\"data\":\"");
   payload += data_readtime;
   payload += "," + String(device_id);
   payload += "|" + temperature;
   payload += "," + relative_humidity;
+  payload += "," + String(soil_humidity);
   payload += "," + String(valve);
   payload += "," + String(pump);
   payload += "," + String(valve_delay);
   payload += "," + String(pump_delay);
+  payload += "," + String(soil_wet_limit);
   payload += "\"}";
 
   payload.toCharArray(msg, 100);
   webSocket.emit("device status", msg);
+  if (reset)
+    lastMillis = millis(); //重置上传计时
+}
 
-  lastMillis = millis(); //重置上传计时
+bool load_config()
+{
+  File configFile = SPIFFS.open("/config.json", "r");
+  if (!configFile)
+  {
+    Serial.println("Failed to open config file");
+    return false;
+  }
+
+  size_t size = configFile.size();
+  if (size > 1024)
+  {
+    Serial.println("Config file size is too large");
+    return false;
+  }
+
+  //Allocate a buffer to store contents of the file.
+  std::unique_ptr<char[]> buf(new char[size]);
+
+  //We don't use String here because ArduinoJson library requires the input
+  //buffer to be mutable. If you don't use ArduinoJson, you may as well
+  //use configFile.readString instead.
+  configFile.readBytes(buf.get(), size);
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject &json = jsonBuffer.parseObject(buf.get());
+
+  if (!json.success())
+  {
+    Serial.println("Failed to parse config file");
+    return false;
+  }
+
+  valve_delay = json["valve_delay"];
+  pump_delay = json["pump_delay"];
+  soil_wet_limit = json["soil_wet_limit"];
+
+  return true;
+}
+
+bool save_config()
+{
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject &json = jsonBuffer.createObject();
+
+  json["valve_delay"] = valve_delay;
+  json["pump_delay"] = pump_delay;
+  json["soil_wet_limit"] = soil_wet_limit;
+
+  File configFile = SPIFFS.open("/config.json", "w");
+  if (!configFile)
+  {
+    Serial.println("Failed to open config file for writing");
+    return false;
+  }
+
+  json.printTo(configFile);
+  return true;
+}
+
+void ISRwatchdog()
+{
+  //看门狗
+  watchdogCount++;
+  if (watchdogCount > 10)
+  {
+    ESP.reset();
+  }
 }
 
 void setup()
@@ -152,53 +259,61 @@ void setup()
   digitalWrite(PUMP_PIN, LOW);
   digitalWrite(VALVE_PIN, LOW);
 
+  SPIFFS.begin(); //配置FS
+  if (!load_config())
+    save_config(); //读取配置，否则保存默认配置
+
   setup_wifi(); //配置WIFI
 
   timeClient.begin(); //NTC服务启动
 
   //OTA设置
-  //Port defaults to 8266
   ArduinoOTA.setPort(8266);
-  //Hostname defaults to esp8266-[ChipID]
   ArduinoOTA.setHostname(device_name);
-  //No authentication by default
-  //ArduinoOTA.setPassword("admin");
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    watchdogCount = 0; //Feed dog while doing update
+  });
   ArduinoOTA.begin();
 
   //WebSocket设置
   webSocket.beginwebsocket(server_url, server_port, "/socket.io/?transport=websocket");
   webSocket.setAuthorization(admin_name, admin_password);
   webSocket.on(device_id, event);
+
+  //Watchdog
+  secondTick.attach(1, ISRwatchdog);
 }
 
 void loop()
 {
-  ArduinoOTA.handle(); // OTA
-  timeClient.update(); // 更新NTP时间
-  webSocket.loop();    // Websocket loop
+  watchdogCount = 0; //Feed dog
 
-  // 每10秒上传一次数据
+  ArduinoOTA.handle(); //OTA
+  timeClient.update(); //更新NTP时间
+  webSocket.loop();    //Websocket loop
+
+  //每10秒上传一次数据
   if (millis() - lastMillis > 10000)
   {
     read_data();
-    upload();
+    upload(1);
   }
 
-  // 延时关闭电磁阀
+  //延时关闭电磁阀
   if (valve_delay_trigger && millis() - valveMillis > 1000 * valve_delay)
   {
     valve_delay_trigger = false;
     valve = false;
     digitalWrite(VALVE_PIN, valve);
-    upload();
+    upload(0);
   }
 
-  // 延时关闭抽水机
+  //延时关闭抽水机
   if (pump_delay_trigger && millis() - pumpMillis > 1000 * pump_delay)
   {
     pump_delay_trigger = false;
     pump = false;
     digitalWrite(PUMP_PIN, pump);
-    upload();
+    upload(0);
   }
 }

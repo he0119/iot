105179b44e1d8d332ea9e6571af08f1e9a31bdc8
Mengyang He <hmy0119@gmail.com>
2018-11-19 11:39:05 +0800

    Move material components into a module

--- angular/iot/src/app/app.module.ts
+++ angular/iot/src/app/app.module.ts
@@ -16,18 +16,7 @@ import { DeviceStatusComponent } from './pages/status/device-status/device-statu
 
 import { KeysPipe } from './shared/keys.pipe';
 
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule, MatMenuModule, MatNativeDateModule } from '@angular/material';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MaterialModule } from './material.module';
 
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
@@ -74,20 +63,7 @@ registerLocaleData(localeZh, 'zh');
       }
     }),
 
-    MatListModule,
-    MatButtonModule,
-    MatMenuModule,
-    MatIconModule,
-    MatSlideToggleModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatDatepickerModule,
-    MatInputModule,
-    MatSelectModule,
-    MatNativeDateModule,
-    MatSnackBarModule,
-    MatExpansionModule,
-    MatCheckboxModule,
+    MaterialModule,
   ],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'zh' },

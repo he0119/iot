6bdb35334c0ad50c175a99e1e6e463e5a9221254
Mengyang He <hmy0119@gmail.com>
2018-11-22 20:10:57 +0800

    Add login and refresh resources and update requirements

--- iot/api/api.py
+++ iot/api/api.py
@@ -2,14 +2,14 @@
 RESTful API&SocketIO
 '''
 from flask import Blueprint
-from flask_login import logout_user
 from flask_restful import Api
 
 from iot.api.resources.devices import Devices
 from iot.api.resources.history import History
 from iot.api.resources.status import Status
-from iot.api.resources.token import Token
 from iot.api.resources.users import Users
+from iot.api.resources.refresh import Refresh
+from iot.api.resources.login import Login
 
 import iot.api.socketio.device
 import iot.api.socketio.website
@@ -18,8 +18,9 @@ api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 #TODO: Use flask-jwt-extended instead of flask-login(socketio remain unchange)
-api.add_resource(Token, '/token')
-api.add_resource(Status, '/status')
-api.add_resource(History, '/history')
-api.add_resource(Users, '/users')
 api.add_resource(Devices, '/devices')
+api.add_resource(History, '/history')
+api.add_resource(Status, '/status')
+api.add_resource(Users, '/users')
+api.add_resource(Refresh, '/refresh')
+api.add_resource(Login, '/login')

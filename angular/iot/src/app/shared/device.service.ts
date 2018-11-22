c098c904100f2de36ed097b4be424f7d1f29c71c
Mengyang He <hmy0119@gmail.com>
2018-11-22 13:21:20 +0800

    Add sessionStorage for deviceInfo

--- angular/iot/src/app/shared/device.service.ts
+++ angular/iot/src/app/shared/device.service.ts
@@ -13,15 +13,35 @@ export class DeviceService {
   constructor(private http: HttpClient) { }
 
   deviceInfo(id) {
+    let key = 'device';
+    key = key + id;
+    let value: Device = JSON.parse(sessionStorage.getItem(key));
+
+    if (value){
+      return of(value);
+    }
+
     const params = new HttpParams()
       .set('id', id);
 
-    return this.http.get<Device>(this.API_URL, { params });
+    return this.http.get<Device>(this.API_URL, { params }).pipe(
+      map(res => {
+        sessionStorage.setItem(key, JSON.stringify(res));
+        return res;
+      })
+    );
   }
 
   devicesInfo() {
+    let value: Device[] = JSON.parse(sessionStorage.getItem("devicesInfo"));
+
+    if (value){
+      return of(value);
+    }
+
     return this.http.get<Device[]>(this.API_URL).pipe(
       map(res => {
+        sessionStorage.setItem("devicesInfo", JSON.stringify(res));
         return res;
       })
     );

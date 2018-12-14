163df2db91b49d48a7866a9b9491242e1020b5df
Mengyang He <hmy0119@gmail.com>
2018-12-14 23:35:47 +0800

    Update andrino code

--- arduino/esp8266/autowatering/config.example.h
+++ arduino/esp8266/autowatering/config.example.h
@@ -1,5 +1,5 @@
 // Config
-const char *device_name = "autowatering"; // 设备名称
+const char *device_name = "auto_watering"; // 设备名称
 
 const char *ssid = "";     // WiFi名称
 const char *password = ""; // WiFi密码
@@ -7,8 +7,8 @@ const char *password = ""; // WiFi密码
 const char *server_url = "127.0.0.1"; // 服务器地址
 const int server_port = 5000;         // 服务器端口
 const int device_id = 1;              // 设备ID，与服务器一致
-#define DHT_VER 11                    // DHT版本 11或者22
-#define ENABLE_SSL 0                  // 是否使用SSL与服务器通讯
+#define DHT_VERSION_22                // DHT版本 11或者22
+// #define ENABLE_SSL                    // 是否使用SSL与服务器通讯
 
 const char *admin_name = ""; // 网页管理员账户
 const char *admin_password = "";
